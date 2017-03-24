87c3d5b8e93143cc7063e9913b77563639e383e1
oburu <oburusolo@gmail.com>
2017-03-24 15:38:43 +0000

    Update status to stop when count reaches 0

--- app/components/countdown.js
+++ app/components/countdown.js
@@ -30,12 +30,20 @@ class Countdown extends Component{
     }
   }
 
+  componentWillUnmount(){
+    clearInterval(this.timer);
+    this.timer=undefined;
+  }
+
   startTimer(){
     this.timer = setInterval(() => {
       let newCount = this.state.count -1;
       this.setState({
         count: newCount >= 0 ? newCount : 0
       });
+      if(newCount === 0){
+        this.setState({countdownStatus: 'stopped'});
+      }
     }, 1000);
   }
 
